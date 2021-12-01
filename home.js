8189d2c1fc2cbe8e6c14374be801e04cd688ec66
Vikitha Reddy <vikitha@umich.edu>
2021-12-01 01:52:29 -0500

    added placeholder text to architecture.html and about.html

--- home.js
+++ home.js
@@ -1,6 +1,5 @@
 function circleOutline() {
     document.getElementById("circle-outline").style.border = "2px dashed #CAE234";
-    console.log("hehe")
     document.getElementById("line-change").style.display = "none";
     document.getElementById("text-under-name").style.display = "inline-block";
 }
@@ -9,3 +8,13 @@ var circle = document.getElementById("circle-outline");
 
 // Code for Chrome, Safari and Opera
 circle.addEventListener("animationend", circleOutline);
+
+function showOneCademy() {
+    document.getElementById("onecademy").style.display = "";
+    document.getElementById("spin").style.display = "none";
+}
+
+function showSpin() {
+    document.getElementById("onecademy").style.display = "none";
+    document.getElementById("spin").style.display = "";
+}
