f8a6ed4f33c450720c4f5190b3447495fda3d3c1
Vikitha Reddy <vikitha@umich.edu>
2021-11-30 20:04:54 -0500

    started working on home page

--- main.js
+++ main.js
@@ -18,9 +18,23 @@ $(window).scroll(function(e) {
     var scroll = $(window).scrollTop();
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         $('.navbar').addClass("navbar-hide");
+        document.getElementById("navbar").style.background = "rgba(255, 255, 255, 0.75)";
     } else {
         $('.navbar').removeClass("navbar-hide");
+        document.getElementById("navbar").style.background = "rgba(255, 255, 255, 0.75)";
     }
 
 });
+
+function circleOutline() {
+    document.getElementById("circle-outline").style.border = "2px dashed #CAE234";
+    console.log("hehe")
+    document.getElementById("line-change").style.display = "none";
+    document.getElementById("text-under-name").style.display = "inline-block";
+}
+
+var circle = document.getElementById("circle-outline");
+
+// Code for Chrome, Safari and Opera
+circle.addEventListener("animationend", circleOutline);
 
