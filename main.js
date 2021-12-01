b4d3c3134a2e2a61685a9cceff2d2fae274c849f
Vikitha Reddy <vikitha@umich.edu>
2021-12-01 00:13:49 -0500

    added about page

--- main.js
+++ main.js
@@ -26,15 +26,4 @@ $(window).scroll(function(e) {
 
 });
 
-function circleOutline() {
-    document.getElementById("circle-outline").style.border = "2px dashed #CAE234";
-    console.log("hehe")
-    document.getElementById("line-change").style.display = "none";
-    document.getElementById("text-under-name").style.display = "inline-block";
-}
-
-var circle = document.getElementById("circle-outline");
-
-// Code for Chrome, Safari and Opera
-circle.addEventListener("animationend", circleOutline);
 
