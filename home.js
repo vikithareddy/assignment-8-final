f4943bdfef0440f606efa61420efd30882feb9c5
Vikitha Reddy <vikitha@umich.edu>
2021-12-02 17:57:57 -0500

    add ucre page

--- home.js
+++ home.js
@@ -14,17 +14,31 @@ circle.addEventListener("animationend", circleOutline);
 function showOneCademy() {
     document.getElementById("onecademy").style.display = "";
     document.getElementById("spin").style.display = "none";
+    document.getElementById("rebox").style.display = "none";
     document.getElementsByClassName("project-names")[0].style.color = "#37303B";
     document.getElementsByClassName("project-names")[1].style.color = "#adadad";
+    document.getElementsByClassName("project-names")[2].style.color = "#adadad";
 }
 
 //show spin project when the project title is clicked and hide 1cademy project
 function showSpin() {
     document.getElementById("spin").style.display = "";
     document.getElementById("onecademy").style.display = "none";
+    document.getElementById("rebox").style.display = "none";
     document.getElementsByClassName("project-names")[1].style.color = "#37303B";
     document.getElementsByClassName("project-names")[0].style.color = "#adadad";
+    document.getElementsByClassName("project-names")[2].style.color = "#adadad";
     // document.getElementsByClassName("project-preview-header").style.color = "transparent";
+}
 
+//show rebox ucre project when the project title is clicked and hide 1cademy project
+function showRebox() {
+    document.getElementById("rebox").style.display = "";
+    document.getElementById("onecademy").style.display = "none";
+    document.getElementById("spin").style.display = "none";
+    document.getElementsByClassName("project-names")[2].style.color = "#37303B";
+    document.getElementsByClassName("project-names")[0].style.color = "#adadad";
+    document.getElementsByClassName("project-names")[1].style.color = "#adadad";
+    // document.getElementsByClassName("project-preview-header").style.color = "transparent";
 }
 
